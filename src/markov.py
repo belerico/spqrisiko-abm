b25b9ed756d2ab1ad8af9fe0b630cdf23da6113b
belerico <belo.fede@outlook.com>
2019-07-30 14:58:46 +0200

    Add comments

--- src/markov.py
+++ src/markov.py
@@ -1,6 +1,16 @@
 import numpy
 from pandas import pandas
 
+"""
+Main reference: https://pdfs.semanticscholar.org/0146/d0d16ea44624c48e4cd7afd1646ed4e90c3d.pdf
+"""
+
+"""
+Let p_ijk be the probability that the defender loses k armies throwing j dice with the attacker 
+throwing i dice.
+The probs dict contains all those probabilities, so for example probs['11'][0] = 21/36 is the
+probability that the defender loses 0 armies throwing 1 dice with the attacker throwing 1 dice
+"""
 probs = {
     '11': [21/36, 5/12],
     '21': [91/216, 125/216],
@@ -9,24 +19,38 @@ probs = {
     '32': [2275/7776, 2611/7776, 2890/7776],
     '33': [17871/46656, 12348/46656, 10017/46656, 6420/46656]
 }
+
+"""
+A is the initial number of attacker's armies, while D is the defender's ones
+"""
 A = 4
 D = 4
 
+"""
+Compute the absorbing and transient states.
+In our case the absorbing states are all those states with a < d, where a and d represent
+the actual attacker and defender armies during a battle respectively
+"""
 absorbing_states = []
 transient_states = []
 idx_col = 0
-for i in range(0, A+1):
-    for j in range(0, D+1):
-        if i == 0 or j == 0 or i < j:
-            if i < j and i > 0 and j > 0:
-                absorbing_states[idx_col:idx_col] = [(i,j)]
+for a in range(0, A+1):
+    for d in range(0, D+1):
+        if a == 0 or d == 0 or a < d:
+            if a < d and a > 0 and d > 0:
+                absorbing_states[idx_col:idx_col] = [(a,d)]
                 idx_col += 1
             else:
-                absorbing_states.append((i,j))
+                absorbing_states.append((a,d))
         else:
-            transient_states.append((i,j))
+            transient_states.append((a,d))
+# Delete the (0,0) state
 del absorbing_states[idx_col]
 
+"""
+Q = matrix of ordered transient states
+R = matrix of ordered absorbing states
+"""
 Q = pandas.DataFrame(
         0,
         columns=transient_states,
@@ -37,22 +61,25 @@ R = pandas.DataFrame(
         columns=absorbing_states,
         index=pandas.MultiIndex.from_tuples(transient_states)
     ) 
-
-for i in range(1, A+1):
-    for j in range(1, D+1):
-        if i >= j:
-            for k, prob in enumerate(probs[str(min(3, i)) + '' + str(min(3, j))]):
+# Filling those matrices with the precomputed porbabilities
+for a in range(1, A+1):
+    for d in range(1, D+1):
+        if a >= d:
+            for k, prob in enumerate(probs[str(min(3, a)) + '' + str(min(3, d))]):
                 try:
-                    Q.loc[(i,j), (i-j+k, j-k)] = prob
+                    Q.loc[(a,d), (a-d+k, d-k)] = prob
                 except KeyError:
-                    R.loc[(i,j), (i-j+k, j-k)] = prob
-
+                    R.loc[(a,d), (a-d+k, d-k)] = prob
+""" 
+Remove all those absorbing states that contains only Oes, since they don't add
+information for the final computation
+"""
 R = R.loc[:, (R != 0).any(axis=0)]
 
 """ idx_col = 0
-for i in range(1, A+1):
-    for j in range(i+1, D+1):
-        state = (i,j)
+for a in range(1, A+1):
+    for d in range(a+1, D+1):
+        state = (a,d)
         #Q.drop(state, axis=0, inplace=True)
         #R.drop(state, axis=0, inplace=True)
         if not R[state].isna().all():
@@ -73,6 +100,7 @@ for state in Q.columns[~Q.isna().all()].tolist():
         R.insert(loc=idx_col, column=state, value=col_to_move)
         idx_col += 1 """
 
+# Theoretical computation: see the main reference
 Q_mat = numpy.asmatrix(Q.to_numpy(), dtype=float)
 R_mat = numpy.asmatrix(R.to_numpy(), dtype=float)
 I = numpy.eye(N=Q_mat.shape[0], M=Q_mat.shape[1])
